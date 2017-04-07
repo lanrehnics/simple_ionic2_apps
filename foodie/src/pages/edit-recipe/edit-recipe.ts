9fa6e28454adc851b252e97eb242ddf6e1c8d34c
Garrett Vangilder <garrett.vangilder@gmail.com>
2017-04-07 07:26:17 -0500

    added foodie app

---
+++ foodie/src/pages/edit-recipe/edit-recipe.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+
+@Component({
+  selector: 'page-edit-recipe',
+  templateUrl: 'edit-recipe.html',
+})
+export class EditRecipePage {
+}
---
+++ foodie/src/pages/recipe/recipe.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+
+@Component({
+  selector: 'page-recipe',
+  templateUrl: 'recipe.html',
+})
+export class RecipePage {
+}
