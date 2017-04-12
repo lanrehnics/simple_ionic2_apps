bd36920ec4ddf0360273f75d2cb18e37dd3ebeb5
Garrett Vangilder <garrett.vangilder@gmail.com>
2017-04-12 07:39:44 -0500

    added world-travler app

---
+++ world-travler/src/app/app.module.ts
@@ -0,0 +1,39 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+import { PlacePage } from '../pages/place/place';
+import { AddPlacePage } from '../pages/add-place/add-place';
+import { SetLocationPage } from '../pages/set-location/set-location';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage,
+    PlacePage,
+    AddPlacePage,
+    SetLocationPage
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage,
+    PlacePage,
+    AddPlacePage,
+    SetLocationPage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
---
+++ world-travler/src/pages/set-location/set-location.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'page-set-location',
+  templateUrl: 'set-location.html',
+})
+export class SetLocationPage {
+}
